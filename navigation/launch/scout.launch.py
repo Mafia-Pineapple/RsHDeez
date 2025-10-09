d447a1b854eb867752a3db54e76bf8bd50e4c634
Calebjc05 <caleb.chadwick1@icloud.com>
2025-10-09 00:44:25 +0000

    Location correct

--- navigation/launch/scout.launch.py
+++ navigation/launch/scout.launch.py
@@ -82,7 +82,7 @@ def generate_launch_description():
                 executable='create',
                 name='spawn_drone',
                 output='screen',
-                arguments=['-name', 'scout', '-topic', 'robot_description', '-y', '0', '-x', '0', '-z', '3.3']
+                arguments=['-name', 'scout', '-topic', 'robot_description', '-y', '-7946.740000', '-x', '-178.273000', '-z', '567.6510']
             )
         ]
     )
